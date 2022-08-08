63d55d514d0a28406cc799f9e5667750c4b337dd
Jeremy <jeremy__francis@hotmail.com>
2022-08-08 09:43:42 -0700

    commit tree 928813e987e01f39c9326da0f5883e3b9a49d9ef

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create([{first_name: 'jake', last_name: 'wilson', grade: 4}, {first_name: 'cody', last_name: 'plantin', grade: 6}, {first_name: 'harry', last_name: 'queslion', grade: 8}])
